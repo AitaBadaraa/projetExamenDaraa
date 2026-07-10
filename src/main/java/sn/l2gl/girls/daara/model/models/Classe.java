d949dbc05e1ce99b9057a3884f14d097664b3dce
Aisha <aichas2005@gmail.com>
2026-07-10 12:12:45 +0100

    Refactor: utilisation de Lombok pour l'entité Classe

--- src/main/java/sn/l2gl/girls/daara/model/models/Classe.java
+++ src/main/java/sn/l2gl/girls/daara/model/models/Classe.java
@@ -1,34 +1,32 @@
 package sn.l2gl.girls.daara.model.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
+// Entité JPA représentant une classe (ex: CI, CP, CE1...) dans le système Daara
 @Entity
 @Table(name = "classes")
+@Getter          // Génère tous les getters (getCode, getLibelle, getNiveau, getMaitre)
+@Setter          // Génère tous les setters (setCode, setLibelle, setNiveau, setMaitre)
+@NoArgsConstructor // Génère le constructeur vide, requis par Hibernate
 public class Classe {
 
+    // Identifiant unique de la classe (clé primaire, ex: "CI-A")
     @Id
     private String code;
 
+    // Nom affiché de la classe (ex: "CI groupe A")
     private String libelle;
 
+    // Niveau scolaire, stocké en base sous forme de texte (String) plutôt que d'entier
     @Enumerated(EnumType.STRING)
     private Niveau niveau;
 
+    // Maître responsable de la classe
+    // optional = false : une classe doit obligatoirement avoir un maître assigné
     @ManyToOne(optional = false)
     @JoinColumn(name = "maitre_id")
     private Maitre maitre;
-
-    public Classe() {}
-
-    public String getCode() { return code; }
-    public void setCode(String code) { this.code = code; }
-
-    public String getLibelle() { return libelle; }
-    public void setLibelle(String libelle) { this.libelle = libelle; }
-
-    public Niveau getNiveau() { return niveau; }
-    public void setNiveau(Niveau niveau) { this.niveau = niveau; }
-
-    public Maitre getMaitre() { return maitre; }
-    public void setMaitre(Maitre maitre) { this.maitre = maitre; }
 }
